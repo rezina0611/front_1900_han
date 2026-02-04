a6a6f30f80df678206e50ca842d438586474d5d7
rezina0611 <han06110@naver.com>
2026-02-04 22:49:25 +0900

    Delete js directory

--- js/resource/js_day06/workspace/day06/object/object.js
+++
@@ -1,8 +0,0 @@
-// 객체의 선언 
-const user1 = new Object();
-const user2 = new Object();
-const user3 = new Object();
-const user4 = {}
-
-user1.name = "홍길동"
-user2.name = "장보고"
--- js/resource/js_day13/workspace/day13/async/async-await.js
+++
@@ -1,8 +0,0 @@
-async function getMyData(){
-  const response = await fetch('https://jsonplaceholder.typicode.com/todos')
-  const todos = await response.json()
-  return todos
-}
-
-const data = await getMyData()
-console.log(data)
